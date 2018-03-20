1ba47bcce34b14e52019e278b208fb6967b9a899
breejesh <breejesh2212@gmail.com>
2018-03-20 16:40:59 +0530

    added profile

---
+++ data/profiles.cpp
@@ -0,0 +1,24 @@
+
+class profile
+{
+    public:
+        uint32_t global_dtb;
+        uint32_t service_dtb;
+}
+
+class win7: public profile
+{
+    public:
+        char process_signature[8] = {3, 0, 88, 0, 0, 0, 0, 0};
+        char service_pattern1[8] = {115, 101, 114, 72, 0, 0, 0, 0};
+        char service_pattern2[8] = {115, 101, 114, 72, 4, 0, 0, 0};
+};
+
+
+class win10: public profile
+{
+    public:
+        char process_signature[8] = {3, 0, 182, 0, 0, 0, 0, 0};
+        char service_pattern1[8] = {115, 101, 114, 72, 0, 0, 0, 0};
+        char service_pattern2[8] = {115, 101, 114, 72, 4, 0, 0, 0};
+};
---
+++ handlers/process_handler.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include "../objects/kernel_module.cpp"
+
+using namespace std;
+
+class process_handler
+{
+    private:
+        vector<kernel_module> modules;
+
+    public:
+        void process_generation()
+        {
+            
+        }
+        
+};
