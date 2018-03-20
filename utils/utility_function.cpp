3ccea0a60e75835aa1cfc73e141d4b4f53c79646
Himanshu <m.himanshu2311@gmail.com>
2018-03-20 15:26:13 +0530

    Initial Commit

---
+++ utils/utility_function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class utility_function
+{
+
+    static void get_utf_string(ifstream &ifile, char str[], uint64_t addr)
+    {
+
+    }
+
+    static int scan_tag(char arr1[], char arr2[], int n)
+    {
+
+    }
+
+    static int compare_array(char arr1[], char arr2[], int n)
+    {
+
+    }
+};
