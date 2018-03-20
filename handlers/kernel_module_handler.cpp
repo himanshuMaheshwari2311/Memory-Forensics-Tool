8af245f6c8ff491d649ced1fbb038147ae4f05af
sagarkamdar <sagarkamdar37@gmail.com>
2018-03-20 15:52:35 +0530

    kernel module and handler

---
+++ handlers/kernel_module_handler.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include "../objects/kernel_module.cpp"
+
+using namespace std;
+
+class kernel_module_handler
+{
+    private:
+        vector<kernel_module> modules;
+
+    public:
+        
+};
---
+++ objects/kernel_module.cpp
@@ -0,0 +1,17 @@
+#include<string>
+#include "object.cpp"
+
+using namespace std;
+
+class kernel_module
+{
+	private:
+		string file_path;
+	public:
+		string getinfo()
+		{
+			string json;
+
+			return json;
+		}
+};
