3720c1b4e355bc134d0b1c56fa4f2ec19c8124db
sagarkamdar <sagarkamdar37@gmail.com>
2018-03-20 16:52:31 +0530

    handler base

--- handlers/kernel_module_handler.cpp
+++ handlers/kernel_module_handler.cpp
@@ -1,14 +1,30 @@
 #include <string>
 #include <vector>
+#include <fstream>
 #include "../objects/kernel_module.cpp"
+#include "../data/profiles.cpp"
 
 using namespace std;
 
 class kernel_module_handler
 {
-    private:
-        vector<kernel_module> modules;
+	private:
+		vector<kernel_module> modules;
 
-    public:
-        
+		
+	public:
+		vector<uint64_t> pool_tag_scan(ifstream file, profile prf)
+		{
+			
+		}
+
+		kernel_module collect_info_module(ifstream file, profile prf, uint64_t phy_offset)
+		{
+
+		}
+
+		void generate_kernel_modules(ifstream file, profile prf)
+		{
+			
+		}
 };
