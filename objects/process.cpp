e4372f49651c3f2af188f13d3485312e82a33822
breejesh <breejesh2212@gmail.com>
2018-03-20 15:45:20 +0530

    process.cpp added

---
+++ objects/process.cpp
@@ -0,0 +1,13 @@
+#include "object.cpp"
+class process: public object
+{
+    private:
+        uint8_t pid;
+        uint8_t ppid;
+        uint8_t active_threads;
+
+    public:
+        string getinfo(){
+            return "This is a Process Object";
+        }
+};
