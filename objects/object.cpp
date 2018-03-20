3d4cf5b70fc9632bfd2871008601306a60f732df
breejesh <breejesh2212@gmail.com>
2018-03-20 15:51:58 +0530

    added timestamp

--- objects/object.cpp
+++ objects/object.cpp
@@ -5,14 +5,26 @@ using namespace std;
 
 enum object_type {process, service, kernal_module, hive, lsa, shin_cache, shell_bags};
 
+typedef
+struct datetime
+{
+    uint8_t day;
+    uint8_t month;
+    uint8_t year;
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+}datetime;
+
 class object
 {
-private:
-    string name;
-    uint64_t physical_offset;
-    uint8_t object_id;
-    enum object_type type; 
+    private:
+        string name;
+        uint64_t physical_offset;
+        uint8_t object_id;
+        enum object_type type; 
+        datetime timestamp;
 
-public:
-    virtual string getinfo()=0;
+    public:
+        virtual string getinfo()=0;
 };
