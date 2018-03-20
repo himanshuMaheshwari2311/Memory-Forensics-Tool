40f88989349d576ce8e3bdcc35830d77282fa65c
breejesh <breejesh2212@gmail.com>
2018-03-20 16:09:31 +0530

    gitignore changed

--- objects/service.cpp
+++ objects/service.cpp
@@ -2,14 +2,15 @@
 
 class process: public object
 {
-    private:
-        string display_name;
-        string driver_name;
-        string process_binary_path;
-        uint8_t pid;
-
-    public:
-        string getinfo(){
+	private:
+		string display_name;
+		string driver_name;
+		string process_binary_path;
+		uint8_t pid;
+	
+	public:
+		string getinfo()
+		{
             return "This is a Service Object";
-        }
+		}
 };
