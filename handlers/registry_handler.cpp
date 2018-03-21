9a7db37f1fe46f134463c3ec066c8619d4039ce5
Himanshu <m.himanshu2311@gmail.com>
2018-03-21 15:03:52 +0530

    Windows 10 data structures

--- handlers/registry_handler.cpp
+++ handlers/registry_handler.cpp
@@ -2,8 +2,9 @@
 #include <string>
 #include <fstream>
 #include <stdint.h>
+#include <iomanip>  
+
 #include "../objects/registry_module.cpp"
-#include "../utils/utility_functions.cpp"
 #include "../data/profiles.cpp"
 
 using namespace std;
@@ -36,14 +37,13 @@ class registry_handler
         registry_module collect_info_module(ifstream &ifile, profile prf, uint64_t phy_offset)
         {
             registry_module rm;
+            char * file_path = new char[128];
             uint64_t addr_val = phy_offset, vir_file_addr, phy_file_addr;
             rm.physical_offset = addr_val;
-            cout<<"Moving to Phy_Offset: "<<phy_offset<<endl;
             ifile.seekg(phy_offset, ios::beg);
             ifile.ignore(1776);
             addr_val += 1776;
             ifile.read(reinterpret_cast<char *>(&vir_file_addr), 8);
-            cout<<hex<<vir_file_addr<<endl;
             addr_val += 8;
             if(vir_file_addr == 0){
                 ifile.ignore(8);
@@ -56,11 +56,12 @@ class registry_handler
             else
             {
                 phy_file_addr = utility_functions::get_phy_addr(ifile, vir_file_addr, 0x00187000); //profiles::get_global_dtb(ifile)
-                cout<<phy_file_addr<<endl;
+                ifile.clear();
+                ifile.seekg(0, ios::beg);
                 ifile.seekg(phy_file_addr, ios::beg);
-                ifile.read(rm.file_path, 64);
-                rm.file_path = utility_functions::get_utf_str(rm.file_path);
-                cout<<rm.file_path<<endl;
+                ifile.read(file_path, 128);
+                rm.file_path = utility_functions::get_utf_str(file_path);
+                cout<<hex<<phy_offset<<setw(64)<<rm.file_path<<endl;
             }
             ifile.clear();
             return rm;
@@ -70,14 +71,10 @@ class registry_handler
         {
             vector<uint64_t> phy_offsets;
             phy_offsets = pool_scan_tag(ifile, prf);
-            for(int i = 0; i < phy_offsets.size(); i++)
-                cout<<hex<<phy_offsets[i]<<endl;
             ifile.clear();
             ifile.seekg(0, ios::beg);
-            cout<<phy_offsets.size()<<" Generating"<<endl;
             for(int i = 0; i < phy_offsets.size(); i++)
             {
-                cout<<"In loop"<<endl;
                 modules.push_back(collect_info_module(ifile, prf, phy_offsets[i]));
             }
         }
