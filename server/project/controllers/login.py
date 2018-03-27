e80ba9e9e58e3afee6fcc56762d0d5a0cb110962
sagarkamdar <sagarkamdar37@gmail.com>
2018-03-27 15:26:18 +0530

    accounts bugfix

--- server/project/controllers/login.py
+++ server/project/controllers/login.py
@@ -2,6 +2,10 @@ from project import app
 from flask import render_template, request
 from flask_wtf import FlaskForm
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template('login/login.html')
+	return render_template('accounts/login.html')
+	if request.method == 'POST':
+		session['username'] = request.form['username']
+		return render_template('login/index.html')
+    return render_template('printer/index.html')
