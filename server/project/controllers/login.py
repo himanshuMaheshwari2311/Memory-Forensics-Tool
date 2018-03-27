1706a6285b3f7897e4186addc5f53aa95e636988
sagarkamdar <sagarkamdar37@gmail.com>
2018-03-27 16:34:37 +0530

    cred complete

--- server/project/controllers/login.py
+++ server/project/controllers/login.py
@@ -1,9 +1,38 @@
 from project import app
-from flask import render_template, request
+from flask import render_template, request, session, flash
 from flask_wtf import FlaskForm
 
+import os
 import json
+
+def credentials(uname, pwd):
+	with open('../data/json/login.json') as json_file:
+		json_data = json.load(json_file)
+		for account in json_data['accounts']:
+			if(account['username'] == uname):
+				if(account['password'] == pwd):
+					return account
+				else:
+					return "pwd"
+		return "uname"
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+	print os.getcwd()
+	
+	if request.method == 'POST':
+		account = credentials(request.form['username'], request.form['password'])
+		
+		if(account == "pwd"):
+			print "Incorrect pwd!"
+			flash("Incorrect password!")
+		elif (account == "uname"):
+			print "Invalid uname!"
+			flash("Invalid username!")
+		else:
+			print account['cases']
+			flash("Logged in!")
+
+		session['username'] = request.form['username']
+		return render_template('accounts/login.html')
 	return render_template('accounts/login.html')
