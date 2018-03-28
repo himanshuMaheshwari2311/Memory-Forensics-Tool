2d21602166910e96a72739c1eb642e85ba3f4c2e
breejesh <breejesh2212@gmail.com>
2018-03-28 14:57:07 +0530

    Login and Case selection done

--- server/project/controllers/case.py
+++ server/project/controllers/case.py
@@ -5,8 +5,8 @@ from flask_wtf import FlaskForm
 import json
 
 @app.route('/case', methods = ['GET', 'POST'])
-def open_case():
+def case():
     if request.method == 'GET':
         case_name = request.args.get('case_name')
-        json.load(open('../data/json/' + case_name))
-        return render_template('case/case.html', **json)
+        case_data = json.load(open('../data/json/' + case_name))
+        return render_template('case/case.html', case_data = case_data)
