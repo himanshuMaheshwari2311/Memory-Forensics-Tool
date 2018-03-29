6b3e77d1c218107302bc6c5a57cd2de7b5ed7bfc
breejesh <breejesh2212@gmail.com>
2018-03-30 00:23:16 +0530

    heavily updated case.html, kept old file as backup

---
+++ server/project/static/js/case.js
@@ -0,0 +1,60 @@
+function initTables() {
+    toggleTable(1);
+}
+function toggleTable(nr) {
+    try {
+        document.getElementById("table1").style.display = "none";
+    }
+    catch(err) {
+        console.log(err);
+    }
+    try {
+        document.getElementById("table2").style.display = "none";
+    }
+    catch(err) {
+        console.log(err);
+    }
+    try {
+        document.getElementById("table3").style.display = "none";
+    }
+    catch(err) {
+        console.log(err);
+    }
+    try {
+        document.getElementById("table4").style.display = "none";
+    }
+    catch(err) {
+        console.log(err);
+    }
+    try {
+        document.getElementById("table5").style.display = "none";
+    }
+    catch(err) {
+        console.log(err);
+    }
+    try {
+        document.getElementById("table6").style.display = "none";
+    }
+    catch(err) {
+        console.log(err);
+    }
+    document.getElementById("table" + nr).style.display = "table-row";
+}
+window.onload = initTables;
+
+function addToReport(id, url) {
+    var comment_msg = prompt("Add some comment regarding this Artifact");
+    if (comment_msg == null || comment_msg == "") {
+        txt = " ";
+    } 
+    console.log(url);
+    $.post(url,
+        {
+            object_id: id,
+            comment: comment_msg
+        },
+        function (data, status) {
+            console.log("btn_" + id);
+            $('#btn_' + id).prop('disabled', true);
+        });
+}
