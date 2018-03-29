161a47207a6e88e62227c77e1314ad4fd247ed1f
sagarkamdar <sagarkamdar37@gmail.com>
2018-03-29 15:09:27 +0530

    python indent

--- server/project/controllers/case.py
+++ server/project/controllers/case.py
@@ -6,11 +6,11 @@ import json
 
 @app.route('/case', methods = ['GET', 'POST'])
 def case():
-    if 'username' not in session:
-        resp = {}
-        resp['result'] = "Please log in!"
-	    return render_template('accounts/login.html', resp = resp)
-    if request.method == 'GET':
-        case_name = request.args.get('case_name')
-        case_data = json.load(open('../data/json/' + case_name))
-        return render_template('case/case.html', case_data = case_data)
+	if 'username' not in session:
+		resp = {}
+		resp['result'] = "Please log in!"
+		return render_template('accounts/login.html', resp = resp)
+	if request.method == 'GET':
+		case_name = request.args.get('case_name')
+		case_data = json.load(open('../data/json/' + case_name))
+		return render_template('case/case.html', case_data = case_data)
