3a7afe5a02a7dee3ca8b196695ce7c3a0d2b1ab6
sagarkamdar <sagarkamdar37@gmail.com>
2018-03-29 16:06:04 +0530

    case file checking fixed

--- server/project/controllers/case.py
+++ server/project/controllers/case.py
@@ -19,6 +19,11 @@ def case():
 			resp['cases'] = session['cases']
 			return render_template('accounts/index.html', resp = resp)
 		try:
+			if case_name not in session['cases']:
+				resp['result_type'] = "danger"
+				resp['result'] = "Specified case name is wrong!"
+				resp['cases'] = session['cases']
+				return render_template('accounts/index.html', resp = resp)
 			case_data = json.load(open('../data/json/' + case_name))
 			return render_template('case/case.html', case_data = case_data)
 		except:
