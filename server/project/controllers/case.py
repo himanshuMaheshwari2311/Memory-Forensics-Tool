2cf79152d7a12332f44775d34e8822dca3da18f0
sagarkamdar <sagarkamdar37@gmail.com>
2018-03-29 15:35:02 +0530

    logged in check, case check

--- server/project/controllers/case.py
+++ server/project/controllers/case.py
@@ -1,16 +1,25 @@
 from project import app
-from flask import render_template, request
+from flask import render_template, request, session
 from flask_wtf import FlaskForm
 
 import json
 
 @app.route('/case', methods = ['GET', 'POST'])
 def case():
+	resp = {}
 	if 'username' not in session:
-		resp = {}
 		resp['result'] = "Please log in!"
 		return render_template('accounts/login.html', resp = resp)
 	if request.method == 'GET':
 		case_name = request.args.get('case_name')
-		case_data = json.load(open('../data/json/' + case_name))
-		return render_template('case/case.html', case_data = case_data)
+		if case_name is None:
+			resp['result'] = "Please specify the case!"
+			resp['cases'] = session['cases']
+			return render_template('accounts/index.html', resp = resp)
+		try:
+			case_data = json.load(open('../data/json/' + case_name))
+			return render_template('case/case.html', case_data = case_data)
+		except:
+			resp['result'] = "Specified case name is wrong!"
+			resp['cases'] = session['cases']
+			return render_template('accounts/index.html', resp = resp)
