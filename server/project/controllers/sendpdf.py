2e4f43c3d5b47d410c72a7d8d356cd3e8a98ec00
sagarkamdar <sagarkamdar37@gmail.com>
2018-03-30 20:47:19 +0530

    update report added

--- server/project/controllers/sendpdf.py
+++ server/project/controllers/sendpdf.py
@@ -10,3 +10,26 @@ def send_report():
 		return send_from_directory('..\\..\\data\\pdfs', 'cl.pdf')
 	except Exception as e:
 		return str(e)
+
+@app.route('/update_report', methods = ['GET', 'POST'])
+def update_report():
+	if request.method == 'POST':
+		overview = request.form['case_overview']
+		acquisition = request.form['case_acquisition']
+		findings = request.form['case_findings']
+		conclusion = request.form['case_conclusion']
+		
+		resp = {}
+		case_data = {}
+		
+		with open('../data/json/' + session['selected_case'], mode='r') as f:
+			case_data = json.load(f)
+			case_data['case_overview'] = overview
+			case_data['case_acquisition'] = acquisition
+			case_data['case_findings'] = findings
+			case_data['case_conclusion'] = conclusion
+		with open('../data/json/' + session['selected_case'], mode='w') as f:
+			json.dump(case_data, f, indent = 4)
+
+		resp['result'] = True
+		return json.dumps(resp)
