c193db9067d0f539dc65b06a27c755ca1047b73e
breejesh <breejesh2212@gmail.com>
2018-03-31 10:33:10 +0530

    UI bug fixes, added report update alert

--- server/project/static/js/case.js
+++ server/project/static/js/case.js
@@ -63,35 +63,40 @@ function addToReport(id, url, table_type, url2) {
             comment: comment_msg
         },
         function (data, status) {
-            console.log(data);
-            console.log("btn_" + id);
-            $('#btn_' + id).prop('disabled', true);
-            try {
-                document.getElementById(table_type + "_tr").style.display = "none";
-            }
-            catch (err) {
-                console.log(err);
-            }
-            data = JSON.parse(data);
-            artifact = data["obj"]
-            console.log(artifact);
-
-            var table = document.getElementById(table_type).getElementsByTagName('tbody')[0];
-            var row = table.insertRow(table.rows.length);
-            var att = document.createAttribute("id");
-            att.value = "tr_" + id;
-            row.setAttributeNode(att);
-            var i = 0;
-            for (var key in artifact) {
-                if (key != "marked" && key != "start" && key != "object_id") {
-                    console.log(artifact[key]);
-                    if (key == "phyical_offset")
-                        row.insertCell(i).innerHTML = artifact[key].toString(16);
-                    else row.insertCell(i).innerHTML = artifact[key];
-                    i++;
+            console.log(data, status);
+            if (status == "success") {
+                console.log("btn_" + id);
+                $('#btn_' + id).prop('disabled', true);
+                try {
+                    document.getElementById(table_type + "_tr").style.display = "none";
                 }
+                catch (err) {
+                    console.log(err);
+                }
+                data = JSON.parse(data);
+                artifact = data["obj"]
+                console.log(artifact);
+
+                var table = document.getElementById(table_type).getElementsByTagName('tbody')[0];
+                var row = table.insertRow(table.rows.length);
+                var att = document.createAttribute("id");
+                att.value = "tr_" + id;
+                row.setAttributeNode(att);
+                var i = 0;
+                for (var key in artifact) {
+                    if (key != "marked" && key != "start" && key != "object_id") {
+                        console.log(artifact[key]);
+                        if (key == "phyical_offset")
+                            row.insertCell(i).innerHTML = artifact[key].toString(16);
+                        else row.insertCell(i).innerHTML = artifact[key];
+                        i++;
+                    }
+                }
+                row.insertCell(i).innerHTML = '<button type="button" class="btn btn-primary" id="btn_d_{{id[0]}}" onclick="removeFromReport(\'' + id + '\', \'' + url2 + '\')" enabled>Remove</button>';
             }
-            row.insertCell(i).innerHTML = '<button type="button" class="btn btn-primary" id="btn_d_{{id[0]}}" onclick="removeFromReport(\'' + id + '\', \'' + url2 + '\')" enabled>Remove</button>';
+            else {
+                alert("Failed to Add");
+            }
         });
 }
 
@@ -106,7 +111,12 @@ function removeFromReport(id, url) {
         },
         function (data, status) {
             console.log("btn_" + id);
-            $('#btn_' + id).prop('disabled', false);
+            if (status == "success") {
+                $('#btn_' + id).prop('disabled', false);
+            }
+            else {
+                alert("Failed to remove");
+            }
         });
 }
 
@@ -126,5 +136,11 @@ function updateReport(id, url) {
         },
         function (data, status) {
             console.log(data);
+            if (status == "success") {
+                alert("Updated Successfully!");
+            }
+            else {
+                alert("Updated Failed");
+            }
         });
 }
